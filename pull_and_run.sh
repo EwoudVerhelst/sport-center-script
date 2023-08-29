90dfff681c0198b59d415a64e0efabf0aaa3f548
Ewoud Verhelst <ewoudverhelst@pop-os.localdomain>
2023-08-29 21:13:14 +0200

    test

--- pull_and_run.sh
+++ pull_and_run.sh
@@ -1,4 +1,4 @@
 cd /home/ubuntu/sport-center-script
 git pull
-/home/ubuntu/sport-center-script/env/bin/python3 /home/ubuntu/sport-center-script/main.py
+docker run --rm -w /usr/workspace -v $(pwd):/usr/workspace raspberry-pi-chromium-webdriver python main.py
 
