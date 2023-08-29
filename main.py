7d87a2f6610148300aba1e90d198ddfea46ee551
Ewoud Verhelst <ewoudverhelst@pop-os.localdomain>
2023-08-29 21:05:31 +0200

    test

--- main.py
+++ main.py
@@ -52,12 +52,18 @@ def init_driver():
 
     # driver = webdriver.Firefox(options=options)
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--ignore-ssl-errors=yes")
-    options.add_argument("--ignore-certificate-errors")
-    driver = webdriver.Remote(
-        command_executor="http://172.18.0.2:4444/wd/hub", options=options
-    )
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--ignore-ssl-errors=yes")
+    # options.add_argument("--ignore-certificate-errors")
+    # driver = webdriver.Remote(
+    #     command_executor="http://172.18.0.2:4444/wd/hub", options=options
+    # )
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
 
     # driver.get(URL)
     return driver
