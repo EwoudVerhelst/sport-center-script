a28bf401ae4159e4e84687d8bb255e86caaa1be2
Ewoud Verhelst <ewoudverhelst@pop-os.localdomain>
2023-08-29 21:07:07 +0200

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 
 #######
 # Date where we want to play volleyball
-TARGET_BOOK_DATE = "24/08/2023"
+TARGET_BOOK_DATE = "30/08/2023"
 SPORT = Sport.BEACHVOLLEY
 PRODUCT = Product.OUTDOOR
 TIME = ["12:00", "13:00"]
